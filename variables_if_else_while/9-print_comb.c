38ff10565ac9038150f721f1f6c5fb7aa870574c
Houcinetb <tiabh16@gmail.com>
2023-10-19 02:13:56 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main -all combination of single digit numberd
+ *
+ * Return: Always 0 (Success)
+ */
+
+int maint(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return  (0);
+}
