f4c5b0ffe9499a821876d8ab6fd24788d9079891
Houcinetb <tiabh16@gmail.com>
2023-10-24 23:38:23 -0700

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 
 /**
- * _puts - prints a string
- * @str: checker
- *
- * Return: Always 0.
- */
+* _puts - prints a string
+* @str: checker
+*
+* i
+* Return: Always 0.
+*/
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str);
+		putchar(*str);
 		str++;
 	}
-	_putchar('\n');
+	putchar('\n');
 }
