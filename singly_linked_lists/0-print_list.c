5337a7bb25bb2e70e7fa27f63c50167ccae11ce9
Houcinetb <tiabh16@gmail.com>
2023-11-28 00:30:05 -0800

    task1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,15 +10,16 @@ size_t print_list(const list_t *h)
 
 {
 	unsigned int count = 0;
-	while(h)
+
+	while (h)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil) \n");
 		else
-			printf("[%u] %s \n",h->len, h->str);
-	h= h->next;
+			printf("[%u] %s \n", h->len, h->str);
+	h = h->next;
 	count++;
 	}
 
-	return(count);
+	return (count);
 }
---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to a list
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+
+{
+
+	unsigned int count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
