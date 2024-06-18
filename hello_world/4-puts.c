1d67264d6f1ce8e2aef7fd022fa7249f3e22b5dd
Houcinetb <tiabh16@gmail.com>
2024-06-18 07:20:08 +0000

    task4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Description: This function prints a phrase using puts.
+ *
+ *Return: Always returns 0 for success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
