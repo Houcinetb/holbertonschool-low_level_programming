0ee7c29a101b62e44de0f4525f133f314fefdd71
Houcinetb <tiabh16@gmail.com>
2024-06-18 07:51:27 +0000

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *
+ *Description: This function prints a phrase using puts.
+ *
+ *Return: Always returns 0 for success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
